83ac291f1b95b9b595d7a9115439aedc7c3da7a2
Tom Gobich <fallofmars@gmail.com>
2017-01-14 21:15:45 -0500

    setup routes needed for authentication and setup very basic auth structure

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('auth', function() {
+    this.route('signup');
+    this.route('login');
+  });
 });
 
 export default Router;
